7866027af24cb24ec0ee393372f12386f90163c9
pezzu <7243796+pezzu@users.noreply.github.com>
2020-12-22 17:16:05 -0500

    Search form: send data to server

--- src/client/components/PatientSearch.js
+++ src/client/components/PatientSearch.js
@@ -1,10 +1,15 @@
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const SearchField = ({ label }) => {
   return (
     <div className="flex m-1">
       <label className="w-1/6 text-lg font-semibold">{label}</label>
-      <input className="w-5/6 border border-gray-300 p-1" name="{label}" />
+      <input
+        className="w-5/6 border border-gray-300 p-1"
+        name={label}
+      />
     </div>
   );
 };
@@ -14,14 +19,40 @@ SearchField.propTypes = {
 };
 
 const PatientSearch = () => {
+  const form = useRef(null);
+  const history = useHistory();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const asString = new URLSearchParams(formData).toString();
+
+    fetch(`/api/v1/patient/?${asString}`, { method: "GET" })
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((found) => history.push(found.id));
+        }
+      })
+      .catch((e) => console.error(e));
+  };
+
+  const handleReset = (event) => {
+    form.current.reset();
+  };
+
   return (
     <div className="w-full max-w-6xl">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+        ref={form}
+      >
         <div className="w-full text-center text-2xl font-bold text-gray-700 mb-6">
           Patient Search
         </div>
         <div className="text-md leading-snug text-justify">
-          <p className="text-red-600 font-semibold mb-2">Description</p>
+          {/* <p className="text-red-600 font-semibold mb-2">Description</p> */}
           <p className="mb-2">
             The FHIR Patient resource defines demographics, care providers, and
             other administrative information about a person receiving care at a
@@ -40,7 +71,7 @@ const PatientSearch = () => {
             <li>Given name, family name, gender, and phone number/email</li>
           </ul>
         </div>
-        <div className="m-4">
+        <div className="mx-4 mt-8">
           <SearchField label="address" />
           <SearchField label="address-city" />
           <SearchField label="address-postalcode" />
@@ -56,11 +87,18 @@ const PatientSearch = () => {
           <SearchField label="partner-prefix" />
           <SearchField label="telecom" />
         </div>
-        <div className="flex text-lg justify-around mt-3">
-          <button className="bg-white border-blue-500 border-2 text-blue-500 font-semibold py-2 px-6 rounded-xl shadow-lg focus:outline-none focus:shadow-outline">
+        <div className="flex text-lg justify-around mt-8">
+          <button
+            type="button"
+            onClick={handleReset}
+            className="bg-white border-blue-500 border-2 text-blue-500 font-semibold py-2 px-6 rounded-xl shadow-lg focus:outline-none focus:shadow-outline"
+          >
             Clear
           </button>
-          <button className="bg-blue-500 border-blue-500 border-2 text-white font-semibold py-2 px-6 rounded-xl shadow-lg focus:outline-none focus:shadow-outline">
+          <button
+            type="submit"
+            className="bg-blue-500 border-blue-500 border-2 text-white font-semibold py-2 px-6 rounded-xl shadow-lg focus:outline-none focus:shadow-outline"
+          >
             Search
           </button>
         </div>
