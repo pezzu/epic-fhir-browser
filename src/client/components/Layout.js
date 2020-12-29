b56625e4fc35cd8ad08dc5be5ce9a19dc144368b
pezzu <7243796+pezzu@users.noreply.github.com>
2020-12-29 13:24:51 -0500

    fit page layout to entire page

--- src/client/components/Layout.js
+++ src/client/components/Layout.js
@@ -1,6 +1,6 @@
 const Layout = (props) => {
   return (
-    <div className="flex justify-center bg-gray-200 h-full">
+    <div className="flex justify-center bg-gray-200 h-full min-h-screen">
       {props.children}
     </div>
   )
